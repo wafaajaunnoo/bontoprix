d960d547668127caa4125bfa1b8774f60423e7cb
Tasneem Bholah <104076030+TasneemHUB@users.noreply.github.com>
2022-06-10 20:43:09 +0400

    Add files via upload

---
+++ header-navigation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation</title>
+    <link rel="stylesheet" href="styles/header-style.css">
+  </head>
+  <body>
+  <div class="nav-container">
+    <nav>
+	<ul>
+      <li><a href="#">Home</li>
+      <li><a href="#">Flights</li>
+      <li><a href="#">Accommodations</li>
+      <li><a href="#">Virtual Experiences</li>
+      <li><a href="#">Stories</li>
+	   <li><a href="#">Contact Us</li>
+     </ul>
+    </nav>
+</div>
+<br><br>
+  </body>
+</html>
